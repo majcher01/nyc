26c1bc6038cfbb40ce9b569bcdb4a4375c279af7
majcher01 <majcherski.jakub@gmail.com>
2022-01-17 19:36:10 +0100

    rzeczy z pendrivea

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var tab1 = [7,3,1,6,9,5,4,10,2,2]; //utworzenie tab1
+
+document.write(tab1[4]+"<br>"); //wyswietlenie 5tej komorki
+document.write('<table>');
+tab1[6]=12;  //zmiana wartosci w 7 komorce
+document.write('<tr><td><b>tab1</b></td>  ');
+
+for(var i=0;i<tab1.length;i++){
+    document.write('<td>'+tab1[i]+'</td>');  //wypisanie calej tablicy
+}
+document.write('</tr>');
+var tab2 = tab1.slice();   //utworzenie tab2 i skopiowanie wartosci
+//document.write('<br><br>');
+
+document.write('<tr><td><b>tab2</b></td>  ');
+for(var i=0;i<tab2.length;i++){
+    document.write('<td>'+tab2[i]+"</td>");  //wyswietlenie calej tab2
+}
+document.write('</tr>');
+
+var tab3=[];
+document.write('<tr><td><b>tab3</b></td>  ');
+for(var i=0;i<tab2.length;i++){
+    tab3[i]=tab1[i]+tab2[i];
+    document.write('<td>'+tab3[i]+"</td>");  //wyswietlenie calej tab3
+
+}
+document.write('</tr>');
+
+tab1=tab2.reverse();
+
+document.write('<tr><td><b>tab1 rev.</b></td>  ');
+
+for(var i=0;i<tab1.length;i++){
+    document.write('<td>'+tab1[i]+"</td>");  //wyswietlenie calej tab2
+}
+document.write('</tr></table>');
+
---
+++ js/main2.js
@@ -0,0 +1,29 @@
+var tab1 = [7,3,1,6,9,5,4,10,2,2]; //utworzenie tab1
+
+document.write(tab1[4]+"<br>"); //wyswietlenie 5tej komorki
+tab1[6]=12;  //zmiana wartosci w 7 komorce
+document.write('<b>tab1</b>  ');
+
+for(var i=0;i<tab1.length;i++){
+    if((2*tab1)>9){document.write(" ");}
+    document.write(tab1[i]+" ");  //wypisanie calej tablicy
+}
+var tab2 = tab1.slice();   //utworzenie tab2 i skopiowanie wartosci
+//document.write('<br><br>');
+
+document.write('<br><br><b>tab2</b>  ');
+
+for(var i=0;i<tab2.length;i++){
+    if((2*tab2)>9){document.write(" ");}
+    document.write(tab2[i]+" ");  //wyswietlenie calej tab2
+}
+
+var tab3=[];
+document.write('<br><br><b>tab3</b>  ');
+for(var i=0;i<tab2.length;i++){
+    tab3[i]=tab1[i]+tab2[i];
+    document.write(tab3[i]+" ");  //wyswietlenie calej tab3
+
+}
+
+
