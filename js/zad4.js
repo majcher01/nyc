543ee07882b40845ffabf545c9bf561621206a6d
majcher01 <majcherski.jakub@gmail.com>
2022-01-19 22:03:32 +0100

    petelka liczaca wystaoienia maxa

--- js/zad4.js
+++ js/zad4.js
@@ -8,6 +8,10 @@ for(var i=0;i<100;i++){
 
 let sort = liczby.sort(function(a, b){return a - b});
 let max = sort[99];
+
+for(var i=0;i<liczby.length;i++){}
+
+
 document.getElementById('output4').innerHTML='max: '+max;
 
 
