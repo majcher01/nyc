0054c1efd91dd1c2da0b49c7c3bd1a0304f79b8e
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-01-21 23:48:04 +0100

    zad5

--- js/zad5.js
+++ js/zad5.js
@@ -1,9 +1,35 @@
 let tablica = new Array(100);
 tablica[0]=0, tablica[1]=1;
-console.log(tablica);
 
 for (var i=2;i<100;i++){
     tablica[i]=(tablica[i-1]+tablica[i-2]);
 }
 
+for (var i =0;i<tablica.length;i++){
+    tablica[i]=Math.pow(2, i);
+}
+
+tablica[0]=3
+for(var i=1;i<tablica.length;i++){
+    tablica[i]=(tablica[i-1]+3);
+}
+
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+
+tablica=[];
+
+var i=3;
+do{
+    if(isPrime(i)==true){
+        tablica.push(i);
+    }
+
+    i++
+}while(tablica.length<100)
 console.log(tablica);
+console.log(isPrime(6))
+  
