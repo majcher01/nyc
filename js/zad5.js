163f2d468f294d69bdbf2595957ce30fa292b2b7
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-01-21 23:57:38 +0100

    dokonczenie zad5, dodanie  komentarzy z autorem do kazdego pliku

--- js/zad5.js
+++ js/zad5.js
@@ -1,35 +1,38 @@
-let tablica = new Array(100);
+let tablica = new Array(100);  //utworzenie tablicy
 tablica[0]=0, tablica[1]=1;
-
+window.onload=function(){
 for (var i=2;i<100;i++){
-    tablica[i]=(tablica[i-1]+tablica[i-2]);
+    tablica[i]=(tablica[i-1]+tablica[i-2]); //wypelnienie tablicy ciagiem fibonnaciego
 }
-
+document.getElementById('output6').innerHTML+='ciag fibonacciego:<br>'+tablica.join(', '); //wyswietlenie danych na ekran
 for (var i =0;i<tablica.length;i++){
-    tablica[i]=Math.pow(2, i);
+    tablica[i]=Math.pow(2, i);      //znalezienie kolejnych poteg liczby 2
 }
-
+document.getElementById('output6').innerHTML+='<br><br>potegi liczby 2:<br>'+tablica.join(', '); //wyswietlenie danych
 tablica[0]=3
 for(var i=1;i<tablica.length;i++){
-    tablica[i]=(tablica[i-1]+3);
+    tablica[i]=(tablica[i-1]+3);        //wypelnienie tablicy ciagiem liczb
 }
-
+document.getElementById('output6').innerHTML+='<br><br> ciag liczb:<br>'+tablica.join(', '); //wyswietlenie
 function isPrime(num) {
-    for(var i = 2; i < num; i++)
+    for(var i = 2; i < num; i++)        //funkcja sprawdzajaca czy liczba jest pierwsza
       if(num % i === 0) return false;
     return num > 1;
   }
 
-tablica=[];
+tablica=[];         //wyczyszczenie tablicy
 
 var i=3;
-do{
+do{                         //wypenienie tablicy liczbami pierwszymi
     if(isPrime(i)==true){
         tablica.push(i);
     }
 
     i++
 }while(tablica.length<100)
-console.log(tablica);
-console.log(isPrime(6))
+document.getElementById('output6').innerHTML+='<br><br> pierwsze 100 liczb pierwszych:<br>'+tablica.join(', '); //wyswietlenie danych na ekran
   
+}
+
+
+// Jakub Majcherski 2022
