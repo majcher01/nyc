9eaed56422ab085a5064ef8e2392160ebfeaba76
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-10 12:20:45 +0100

    zrobione zadanie 1 i 2

---
+++ tablice-1003/js/drugie.js
@@ -0,0 +1,14 @@
+let kolory2=["czerwony","zielony","niebieski"];
+document.write("przed zmiana: <br>");
+
+for(var i=0;i<kolory2.length;i++){
+    document.write(kolory2[i]+" ");
+}
+
+kolory2=["bialy","fioloetowy","zolty"];
+
+document.write("<br> po zmianie: <br>");
+
+for(var i=0;i<kolory2.length;i++){
+    document.write(kolory2[i]+" ");
+}
