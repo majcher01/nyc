c0f7616dd6208aede760416fb722106aa21dae84
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-17 12:59:34 +0100

    nie wiem

---
+++ window-1703/js/czwarte.js
@@ -0,0 +1,16 @@
+
+
+function zad4(){
+
+    let interwal=setInterval(func2, 1000);
+
+    function func2(){
+        var x=1;
+
+        document.getElementById("interwal").innerHTML+=" "+x;
+    }
+
+
+
+
+}
---
+++ window-1703/js/trzecie.js
@@ -0,0 +1,5 @@
+window.setTimeout(func1, 5000);
+
+function func1(){
+    alert("mielo 5 sekund");
+}
