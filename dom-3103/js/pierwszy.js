82affe2613801f63ad202ef63164e16b51cb05bd
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-03-31 12:18:32 +0200

    zad1

--- dom-3103/js/pierwszy.js
+++ dom-3103/js/pierwszy.js
@@ -1,2 +1,9 @@
 let tytul = document.title;
+let liczbazdjec = document.images.length;
+let liczbaodnosnikow = document.links.length;
+let liczbaformularzy = document.forms.length;
+
 document.getElementById("tytul").innerHTML="Tytuł strony: "+tytul;
+document.getElementById("zdjecia").innerHTML="Liczba zdjec: "+liczbazdjec;
+document.getElementById("odnosniki").innerHTML="Liczba odnosnikow: "+liczbaodnosnikow;
+document.getElementById("formularze").innerHTML="Liczba formularzy: "+liczbaformularzy;
