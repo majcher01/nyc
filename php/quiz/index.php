743984d1a2c1f7ff81aa31dc71625e1f97f66eb0
majcher01 <majcherski.jakub@gmail.com>
2023-03-20 13:00:39 +0100

    start quiza

---
+++ php/quiz/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Jakub Majcherski</title>
+    <style>
+        .srodek{
+            display: flex;
+            justify-content: center;
+            margin-top:150px;
+        }
+        .przycisk{width: 150px;
+height: 50px;
+font-size: 20px;
+background-color: transparent;
+border: 1px solid black;
+border-radius: 15px;}
+            .przycisk:hover{
+                background-color: black;
+                color:white;
+                transition-duration: 0.4s;
+            }
+        </style>
+</head>
+<body>
+
+<div class='srodek'>
+    <div style='text-align: center;'>
+    <div>
+    Quiz PHP Jakub Majcherski
+    </div>
+    <div style='margin-top:40px;'>
+        <a href='login.php'><button class='przycisk'>Logowanie</button></a>
+        <br><br>
+        <a href='register.php'><button class='przycisk'>Rejestracja</button></a>
+    </div>
+</div>
+
+</div>
+    
+</body>
+</html>
---
+++ php/quiz/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Jakub Majcherski</title>
+    <style>
+        .srodek{
+            display: flex;
+            justify-content: center;
+            margin-top:150px;
+        }
+        .przycisk{width: 150px;
+height: 50px;
+font-size: 20px;
+background-color: transparent;
+border: 1px solid black;
+border-radius: 15px;}
+            .przycisk:hover{
+                background-color: black;
+                color:white;
+                transition-duration: 0.4s;
+            }
+        </style>
+</head>
+<body>
+
+<div class='srodek'>
+    <div style='text-align: center;'>
+    <div>
+    Logowanie
+    </div>
+    <div style='margin-top:40px;'>
+    <form action='php/valid.php' method='post'>
+            E-mail:
+            <input type='email' placeholder='email' name='email'>
+            <br><br>
+            Haslo:
+            <input type='password' placeholder='Haslo' name='haslo'>
+            <br><br>
+            <input type='submit' value='Potwierdz'>
+        </form>
+    </div>
+</div>
+
+</div>
+    
+</body>
+</html>
