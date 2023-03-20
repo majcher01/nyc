7e847046098e68746e92163194d83f68cfd91eab
majcher01 <majcherski.jakub@gmail.com>
2023-03-20 23:47:25 +0100

    niewiem

--- php/quiz/profile/index.php
+++ php/quiz/profile/index.php
@@ -59,7 +59,8 @@ table {
   color: white;
   cursor: pointer;
   padding: 18px;
-  width: 100%;
+  /*width: 100%;*/
+  width:700px;
   border: none;
   text-align: left;
   outline: none;
@@ -71,7 +72,6 @@ table {
 }
 
 .collapsible:after {
-  content: '\002B';
   color: white;
   font-weight: bold;
   float: right;
@@ -83,12 +83,12 @@ table {
 }
 
 .content {
-  padding-top:18px;
+  padding-top:0px;
   max-height: 0;
   overflow: hidden;
   transition: max-height 0.2s ease-out;
   background-color: #f1f1f1;
-  width:736px;
+  /*width:736px;*/
 }
 .tb{
   display: inline-block;
@@ -115,12 +115,14 @@ Twoje poprzednie wyniki:
 $uid=$result['id'];
 $r2=mysqli_query($connection, " SELECT * FROM wyniki WHERE userid='$uid' ");
 echo "
+<div>
 <span class='tb'>Data</span>
 <span class='tb'>Czas rozpoczęcia</span>
 <span class='tb'>Czas zakończenia</span>
 <span class='tb'>Czas trwania</span>
 <span class='tx'>Wynik</span>
 <span class='tx'></span>
+</div>
 <br><br>";
 while($row=mysqli_fetch_array($r2)){
   /*
@@ -132,7 +134,7 @@ echo "<td>".$row['czas-trwania']." minuty</td>";
 echo "<td>".$row['wynik']."/5</td>";
 echo"</tr>";
 */
-echo "<span class='collapsible'>";
+echo "<div class='collapsible'>";
 echo "<span class='tb'>".$row['data']."</span>";
 echo "<span class='tb'>".$row['czas-rozpoczecia']."</span>";
 echo "<span class='tb'>".$row['czas-zakonczenia']."</span>";
@@ -140,11 +142,85 @@ echo "<span class='tb'>".$row['czas-trwania']." minuty</span>";
 echo "<span class='tx'>".$row['wynik']."/5</span>";
 echo "<span class='tx'>"."Rozwiń"."</span>";
 echo"
-</span>
+</div>
 <div class='content'>
-Błędne odpowiedzi:
+<br>
+<B>Błędne odpowiedzi:</b>
+<br><br>
+";
 
+$idw=$row['id'];
 
+$p1=mysqli_query($connection, " SELECT pytania.tresc, wyniki.odp1, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp1=pytania.id WHERE wyniki.id='$idw'; ");
+$rp1=mysqli_fetch_array($p1);
+$p2=mysqli_query($connection, " SELECT pytania.tresc, wyniki.odp2, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp2=pytania.id WHERE wyniki.id='$idw'; ");
+$rp2=mysqli_fetch_array($p2);
+$p3=mysqli_query($connection, " SELECT pytania.tresc, wyniki.odp3, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp3=pytania.id WHERE wyniki.id='$idw'; ");
+$rp3=mysqli_fetch_array($p3);
+$p4=mysqli_query($connection, " SELECT pytania.tresc, wyniki.odp4, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp4=pytania.id WHERE wyniki.id='$idw'; ");
+$rp4=mysqli_fetch_array($p4);
+$p5=mysqli_query($connection, " SELECT pytania.tresc, wyniki.odp5, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp5=pytania.id WHERE wyniki.id='$idw'; ");
+$rp5=mysqli_fetch_array($p5);
+
+if($rp1['odp1']!=$rp1['odppoprawna']){
+  echo "Pytanie: <br>".$rp1['tresc']."<br><br>";
+  $trescp1=$rp1['tresc'];
+  $odpusera=mysqli_fetch_array(mysqli_query($connection, " SELECT * FROM pytania WHERE tresc='$trescp1'; "));
+  $t1="odp".$rp1['odp1'];
+  echo "Twoja odpowiedź: <br><span style='color:red;'>".$odpusera[$t1]."</span><br>";
+  $t2="odp".$rp1['odppoprawna'];
+  echo "Poprawna odpowiedź: <br><span style='color:green;'>".$odpusera[$t2]."</span><br>";
+  echo "<br><br>";
+
+}
+
+if($rp2['odp2']!=$rp2['odppoprawna']){
+  echo "Pytanie: <br>".$rp2['tresc']."<br><br>";
+  $trescp2=$rp2['tresc'];
+  $odpusera=mysqli_fetch_array(mysqli_query($connection, " SELECT * FROM pytania WHERE tresc='$trescp2'; "));
+  $t1="odp".$rp2['odp2'];
+  echo "Twoja odpowiedź: <br><span style='color:red;'>".$odpusera[$t1]."</span><br>";
+  $t2="odp".$rp2['odppoprawna'];
+  echo "Poprawna odpowiedź: <br><span style='color:green;'>".$odpusera[$t2]."</span><br>";
+  echo "<br><br>";
+
+}
+
+if($rp3['odp3']!=$rp3['odppoprawna']){
+  echo "Pytanie: <br>".$rp3['tresc']."<br><br>";
+  $trescp3=$rp3['tresc'];
+  $odpusera=mysqli_fetch_array(mysqli_query($connection, " SELECT * FROM pytania WHERE tresc='$trescp3'; "));
+  $t1="odp".$rp3['odp3'];
+  echo "Twoja odpowiedź: <br><span style='color:red;'>".$odpusera[$t1]."</span><br>";
+  $t2="odp".$rp3['odppoprawna'];
+  echo "Poprawna odpowiedź: <br><span style='color:green;'>".$odpusera[$t2]."</span><br>";
+  echo "<br><br>";
+
+}
+
+if($rp4['odp4']!=$rp4['odppoprawna']){
+  echo "Pytanie: <br>".$rp4['tresc']."<br><br>";
+  $trescp4=$rp4['tresc'];
+  $odpusera=mysqli_fetch_array(mysqli_query($connection, " SELECT * FROM pytania WHERE tresc='$trescp4'; "));
+  $t1="odp".$rp4['odp4'];
+  echo "Twoja odpowiedź: <br><span style='color:red;'>".$odpusera[$t1]."</span><br>";
+  $t2="odp".$rp4['odppoprawna'];
+  echo "Poprawna odpowiedź: <br><span style='color:green;'>".$odpusera[$t2]."</span><br>";
+  echo "<br><br>";
+
+}
+if($rp5['odp5']!=$rp5['odppoprawna']){
+  echo "Pytanie: <br>".$rp5['tresc']."<br><br>";
+  $trescp5=$rp5['tresc'];
+  $odpusera=mysqli_fetch_array(mysqli_query($connection, " SELECT * FROM pytania WHERE tresc='$trescp5'; "));
+  $t1="odp".$rp5['odp5'];
+  echo "Twoja odpowiedź: <br><span style='color:red;'>".$odpusera[$t1]."</span><br>";
+  $t2="odp".$rp5['odppoprawna'];
+  echo "Poprawna odpowiedź: <br><span style='color:green;'>".$odpusera[$t2]."</span><br>";
+  echo "<br><br>";
+
+}
+echo"
 </div>
 ";
 //SELECT pytania.tresc, wyniki.odp1, pytania.odppoprawna FROM `wyniki` JOIN pytania ON wyniki.idp1=pytania.id;
