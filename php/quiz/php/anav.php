a6faaa576a2ccf48fe67b307c697baf951ffaaf0
majcher01 <majcherski.jakub@gmail.com>
2023-03-24 13:29:40 +0100

    tabeleczka z uzytkownikami

--- php/quiz/php/anav.php
+++ php/quiz/php/anav.php
@@ -1,6 +1,7 @@
 <div style='margin-bottom:20px; border-bottom:2px solid black; height:60px; padding: 5px 5px 5px 5px;'>
     <span style='float:left;' class="navel"><a href='.' style='text-decoration:none;'>Podsumowanie</a></span>
-    <span style='float:left;' class="navel"><a href='./Użytkownicy' style='text-decoration:none;'>Użytkownicy</a></span>
+    <span style='float:left;' class="navel"><a href='./uzytkownicy' style='text-decoration:none;'>Użytkownicy</a></span>
+    <span style='float:left;' class="navel"><a href='./wyniki' style='text-decoration:none;'>Wyniki</a></span>
     <span style='float:left;' class="navel"><a href='./pytania' style='text-decoration:none;'>Pytania</a></span>
     <span style='float:left;' class="navel"><a href='../app/' style='text-decoration:none;'>Quiz</a></span>
     <span style='float:left;' class="navel"><a href='../profile' style='text-decoration:none;'>Profil</a></span>
