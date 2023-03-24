c76515273df2bc86eab533f041d5f2de8e589a2d
majcher01 <majcherski.jakub@gmail.com>
2023-03-24 09:09:32 +0100

    ladnejszy wynik w quizcheck

--- php/quiz/php/quizcheck.php
+++ php/quiz/php/quizcheck.php
@@ -57,7 +57,6 @@ $o5=$selected[4];
 
 mysqli_query($connection, " INSERT INTO `wyniki` (`id`, `userid`, `data`, `czas-rozpoczecia`, `czas-zakonczenia`, `czas-trwania`, `wynik`, `idp1`, `odp1`, `idp2`, `odp2`, `idp3`, `odp3`, `idp4`, `odp4`, `idp5`, `odp5`) VALUES (NULL, '$userid', '$data', '$czasrozpoczecia', '$czaszakonczenia', '$diff_minutes', '$poprawne', '$selected[0]', '$_POST[$o1]', '$selected[1]', '$_POST[$o2]', '$selected[2]', '$_POST[$o3]', '$selected[3]', '$_POST[$o4]', '$selected[4]', '$_POST[$o5]');  ");
 
-echo "Liczba poprawnych: ". $poprawne;
 
 
 $connection->close();
@@ -66,3 +65,48 @@ $connection->close();
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wyniki</title>
+    <style>
+.przycisk{width: 150px;
+height: 50px;
+font-size: 20px;
+background-color: transparent;
+border: 1px solid black;
+border-radius: 15px;}
+            .przycisk:hover{
+                background-color: black;
+                color:white;
+                transition-duration: 0.4s;
+            }
+            .srodek{
+            display: flex;
+            justify-content: center;
+            margin-top:150px;
+        }
+
+</style>
+</head>
+<body>
+    <div class='srodek'>
+    <div style='text-align: center;'>
+    <div>
+    Liczba poprawnych odpowiedzi: <?php echo $poprawne."/5";?>
+    <br>
+    Aby zobaczyć historię wyników przejdź do swojego profilu.
+    </div>
+    <div style='margin-top:40px;'>
+        <a href='../profile/'><button class='przycisk'>Profil</button></a>
+        <br><br>
+        <a href='../app/'><button class='przycisk'>Quiz</button></a>
+    </div>
+</div>
+
+</div>
+</body>
+</html>
